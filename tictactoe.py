d5c87f3cea7f22e7335c8a93f92a7738d79a25dd
saralotfi <saralotfi3442@gmail.com>
2024-07-10 12:43:29 +0330

    .

--- tictactoe.py
+++ tictactoe.py
@@ -1,70 +1,70 @@
 from os import system
+
 class TicTacToe:
     def __init__(self):
-        self.board = self.initialBoard()
+        self.initialBoard()
         self.current_player = 'X'
 
     def initialBoard(self):
-        board = []
+        self.board = []
         for _ in range(3):
             row = ['-'] * 3
-            board.append(row)
-        return board
+            self.board.append(row)
 
     def draw(self):
         for row in self.board:
             print(' '.join(row))
         print()
 
-    def isfull(self):
+    def is_board_full(self):
         for row in self.board:
             if '-' in row:
                 return False
         return True
 
-    def all_equal(self, iterator, player):
+    def all_equal(iterator, player):
         for cell in iterator:
             if cell != player:
                 return False
         return True
 
-    def get_diagonal(self, main=True):
+    def get_diagonal(main=True):
         if main:
             return (self.board[i][i] for i in range(3))
         else:
             return (self.board[i][2 - i] for i in range(3))
 
-    def check_rows(self, player):
+    def check_rows(player):
         for row in self.board:
             if self.all_equal(row, player):
                 return True
         return False
 
-    def check_columns(self, player):
+    def check_columns(player):
         for col in range(3):
             if self.all_equal((self.board[row][col] for row in range(3)), player):
                 return True
         return False
 
-    def check_diagonals(self, player):
+    def check_diagonals(player):
         if self.all_equal(self.get_diagonal(main=True), player) or self.all_equal(self.get_diagonal(main=False), player):
             return True
         return False
 
-    def iswin(self, player):
+    def iswin(player):
         return self.check_rows(player) or self.check_columns(player) or self.check_diagonals(player)
 
     def playturn(self):
-        while not self.isfull():
+        while not self.is_board_full():
             self.draw()
             if not self.take_player_turn():
                 continue
-            
+
             if self.iswin(self.current_player):
                 self.draw()
                 print(f"{self.current_player} is win!")
                 return
-            system( 'cls' )
+            system('cls' if os.name == 'nt' else 'clear')
             self.current_player = 'O' if self.current_player == 'X' else 'X'
         
         self.draw()
@@ -87,9 +87,10 @@ class TicTacToe:
 
 def start_game():
     game = TicTacToe()
-    game.play_turn()
+    game.playturn()
 
 start_game()
+
 
 
 
